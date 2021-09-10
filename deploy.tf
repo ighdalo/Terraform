d8c77f641f0ee900d5fbbe567f3d043d072a1129
ighdalo <51100322+ighdalo@users.noreply.github.com>
2021-09-09 23:19:27 -0400

    Delete deploy.tf

--- deploy.tf
+++
@@ -1,8 +0,0 @@
-module "ec2_test" {
-  source        = "/home/labadmin/TerraformProject/modules/ec2_setup"
-  ec2_count     = 2
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.micro"
-  server_name   = "test_server"
-
-}
